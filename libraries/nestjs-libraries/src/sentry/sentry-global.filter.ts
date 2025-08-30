da95d42ee34c96084666fb640096179d5e5eccb2
Josemi Castro <josemi.inficonglobal@gmail.com>
2025-08-30 13:30:28 +0200

    Update sentry-global.filter.ts

--- libraries/nestjs-libraries/src/sentry/sentry-global.filter.ts
+++ libraries/nestjs-libraries/src/sentry/sentry-global.filter.ts
@@ -1,1 +1,32 @@
 
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { HttpAdapterHost } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
+
+@Catch()
+export class SafeSentryFilter extends BaseExceptionFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {
+    super(httpAdapterHost.httpAdapter);
+  }
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+
+    const userId =
+      typeof request.getUser === 'function'
+        ? request.getUser()?.id
+        : undefined;
+
+    // 🔐 Sentry captura sin romper rutas públicas
+    Sentry.withScope(scope => {
+      if (userId) {
+        scope.setUser({ id: userId });
+      }
+      Sentry.captureException(exception);
+    });
+
+    super.catch(exception, host);
+  }
+}
